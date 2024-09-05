a32f28c692061ae44d2e675ca2cee20d3bec131a
sMarkewitz <simon.markewitz@abs-it-kiel.de>
2024-09-05 10:08:50 +0200

    first commit

---
+++ kitchenscreen/src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer app height="40">
+    footer
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ kitchenscreen/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>huhu</div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
